065c048051543eec1a586cad0824386ea16e43ce
kubpi <jakub.pietyszuk@gmail.com>
2023-10-13 22:27:18 +0200

    First commit

---
+++ MainViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Grafika4
+{
+    public class MainViewModel
+    {
+        public ObservableCollection<RectangleViewModel> Rectangles { get; set; }
+        public ICommand AddRectangleCommand { get; }
+
+        public MainViewModel()
+        {
+            Rectangles = new ObservableCollection<RectangleViewModel>();
+            AddRectangleCommand = new RelayCommand(AddRectangle);
+        }
+
+        private void AddRectangle()
+        {
+            var newRectangle = new RectangleViewModel
+            {
+                Position = Mouse.GetPosition(Application.Current.MainWindow),
+                Width = 50,
+                Height = 50
+            };
+
+            Rectangles.Add(newRectangle);
+        }
+    }
+}
---
+++ RectangleViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace Grafika4
+{
+    public class RectangleViewModel
+    {
+        public Point Position { get; set; }
+        public double Width { get; set; }
+        public double Height { get; set; }
+    }
+}
